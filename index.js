396aa017ce2c6e6c4d2dcb6a67f10c9aa735cb70
Vasilyev Dmitry <vaseker@yandex-team.ru>
2014-11-19 00:55:36 +0500

    Added sticky-session

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
-var cluster = require('cluster'),
-    numCPUs = require('os').cpus().length;
-
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var redis = require('socket.io-redis');
-io.adapter(redis({host: 'localhost', port: 6379}));
-
+var numCPUs = require('os').cpus().length,
+    app = require('express')();
 
 app.get('/', function (req, res) {
     res.sendfile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
-    socket.on('chat message', function (msg) {
-        io.emit('chat message', msg);
+var sticky = require('sticky-session');
+
+var redis = require('socket.io-redis');
+
+sticky(numCPUs, function() {
+    // This code will be executed only in slave workers
+
+    var http = require('http').Server(app);
+    var io = require('socket.io')(http);
+
+    io.adapter(redis({host: 'localhost', port: 6379}));
+
+    io.on('connection', function (socket) {
+        console.log('a user connected to PID ' + process.pid);
+
+        socket.on('chat message', function (msg) {
+            io.emit('chat message', msg);
+        });
     });
+
+    return http;
+}).listen(3000, function() {
+    console.log('server ' + process.pid + ' started on 3000 port');
 });
-
-if (cluster.isMaster) {
-    for (var i = 0; i < numCPUs; i++) {
-        cluster.fork();
-    }
-
-    cluster.on('disconnect', function (worker) {
-        console.error(worker.process.pid + ' disconnected!');
-        cluster.fork();
-    });
-
-} else {
-    http.listen(3000, function () {
-        console.log('listening on *:3000');
-    });
-}
